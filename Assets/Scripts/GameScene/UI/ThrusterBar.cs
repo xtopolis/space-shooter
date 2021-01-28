45cd6955175a03a4eb0d2b3b7ac59aa2bed2899b
Xtops <>
2021-01-27 17:37:06 -0800

    Phase 1 - Thruster Scaling

---
+++ Assets/Scripts/GameScene/UI/ThrusterBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThrusterBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetAmount(float amount)
+    {
+        slider.value = amount;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+    public void SetMaxAmount(float amount)
+    {
+        slider.maxValue = amount;
+        slider.value = amount;
+
+        fill.color = gradient.Evaluate(1f);
+    }
+}
